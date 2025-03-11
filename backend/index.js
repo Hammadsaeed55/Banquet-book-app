9846c096f2314b75131c911699ba633f826ef7a2
Hammad saeed <hammadsaeed6702@gmail.com>
2025-03-11 12:31:52 -0700

    add some required folder such as authControllers, authRoutes

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const connectDB = require('./config/connection')
+const cors = require('cors');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
+// Load environment variables
+dotenv.config();
+
+// Connect to the database
 connectDB();
+
+app.use(cors());
 app.use(express.json());
 
-app.get('/',(req, res)=>{
-    res.send("hello kya haal hai");
+// Use the imported routes
+app.use('/api/auth', authRoutes)
+
+// Example route
+app.get('/', (req, res) => {
+    res.send("API is running...");
 })
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, (req, res)=>{
-    console.log("server start 👍");
+app.listen(PORT, (req, res) => {
+    console.log(`Server running 👍 on port ${PORT}`);
 })
